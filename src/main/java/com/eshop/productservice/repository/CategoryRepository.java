e4f50fc60e57d97deef0456caf4199f43c131fb4
Elansari-Jaafar <elansarijaafar@gmail.com>
2024-05-01 21:18:33 +0100

    Add new get functions

--- src/main/java/com/eshop/productservice/repository/CategoryRepository.java
+++ src/main/java/com/eshop/productservice/repository/CategoryRepository.java
@@ -1,17 +1,13 @@
 package com.eshop.productservice.repository;
 
-import com.eshop.productservice.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eshop.productservice.model.Category;
 
-import java.util.Optional;
-
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByName(String name);
     boolean existsById(long Id);
     Page<Category> findAll(Pageable pageable);
-
 }
